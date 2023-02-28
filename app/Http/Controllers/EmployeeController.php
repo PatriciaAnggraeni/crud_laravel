31986733ccadb92e42a6f6090f9e101e7c27cbb3
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-02-28 20:29:16 +0700

    mengambil data dari database dan meng-dd datanya

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
     // membuat fungsi index untuk me-return view (tampilan untuk data pegawai)
     public function index() {
+
+        // mengambil semua data di tabel employee
+        $data_pegawai = Employee::all();
+
+        // die dump datanya menggunakan fungsi dd()
+        dd($data_pegawai);
 
         // return file blade.php dengan nama pegawai
         return view('pegawai');
